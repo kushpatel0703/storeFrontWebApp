ad7c013b263c3ad75a85f219499f20b0048110de
kushpatel0703 <kushpatel0703@gmail.com>
2018-11-14 16:49:24 -0600

    Base File and Linking Setup

---
+++ storeFrontWebApp/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for storeFrontWebApp project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'storeFrontWebApp.settings')
+
+application = get_wsgi_application()
